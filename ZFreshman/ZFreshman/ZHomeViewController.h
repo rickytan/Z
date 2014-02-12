b5da68769dbb409ee2929352db9b5261af62aaa9
rickytan <ricky.tan.xin@gmail.com>
2014-02-12 22:34:17 +0800

    zfresh splash page. init code

---
+++ ZFreshman/ZFreshman/ZHomeViewController.h
@@ -0,0 +1,13 @@
+//
+//  ZHomeViewController.h
+//  ZFreshman
+//
+//  Created by ricky on 14-2-12.
+//  Copyright (c) 2014年 Ricky. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ZHomeViewController : UICollectionViewController
+
+@end
---
+++ ZFreshman/ZFreshman/ZHomeViewController.m
@@ -0,0 +1,58 @@
+//
+//  ZHomeViewController.m
+//  ZFreshman
+//
+//  Created by ricky on 14-2-12.
+//  Copyright (c) 2014年 Ricky. All rights reserved.
+//
+
+#import "ZHomeViewController.h"
+
+@interface ZHomeViewController ()
+
+@end
+
+@implementation ZHomeViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (NSInteger)numberOfSectionsInCollectionView:(UICollectionView *)collectionView
+{
+    return 1;
+}
+
+- (NSInteger)collectionView:(UICollectionView *)collectionView
+     numberOfItemsInSection:(NSInteger)section
+{
+    return 7;
+}
+
+- (UICollectionViewCell *)collectionView:(UICollectionView *)collectionView
+                  cellForItemAtIndexPath:(NSIndexPath *)indexPath
+{
+    UICollectionViewCell *cell = [collectionView dequeueReusableCellWithReuseIdentifier:@"ItemCell"
+                                                                           forIndexPath:indexPath];
+
+    return cell;
+}
+
+@end

ac1b6c2922b45af59ffeb4b175f2e0142f3f5519
rickytan <ricky.tan.xin@gmail.com>
2014-02-13 16:38:58 +0800

    league files

---
+++ ZFreshman/ZFreshman/ZImageListCell.h
@@ -0,0 +1,13 @@
+//
+//  ZImageListCell.h
+//  ZFreshman
+//
+//  Created by ricky on 14-2-13.
+//  Copyright (c) 2014年 Ricky. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ZImageListCell : UITableViewCell
+
+@end
---
+++ ZFreshman/ZFreshman/ZImageListCell.m
@@ -0,0 +1,29 @@
+//
+//  ZImageListCell.m
+//  ZFreshman
+//
+//  Created by ricky on 14-2-13.
+//  Copyright (c) 2014年 Ricky. All rights reserved.
+//
+
+#import "ZImageListCell.h"
+
+@implementation ZImageListCell
+
+- (id)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier
+{
+    self = [super initWithStyle:style reuseIdentifier:reuseIdentifier];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated
+{
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end

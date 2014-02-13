396c45b3e74f2d435593df38b83d2bd0bc2b2aef
rickytan <ricky.tan.xin@gmail.com>
2014-02-13 17:16:38 +0800

    league

---
+++ ZFreshman/ZFreshman/ZLeagueViewController.h
@@ -0,0 +1,13 @@
+//
+//  ZLeagueViewController.h
+//  ZFreshman
+//
+//  Created by ricky on 14-2-13.
+//  Copyright (c) 2014年 Ricky. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ZLeagueViewController : UITableViewController
+
+@end

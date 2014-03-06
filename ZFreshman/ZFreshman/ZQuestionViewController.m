a514283dff12fe6b1963abf29e3c421f15d94e2c
rickytan <ricky.tan.xin@gmail.com>
2014-03-07 00:18:53 +0800

    zfreshman done!

--- ZFreshman/ZFreshman/ZQuestionViewController.m
+++ ZFreshman/ZFreshman/ZQuestionViewController.m
@@ -10,8 +10,10 @@
 #import "SVProgressHUD.h"
 #import "ZWebViewController.h"
 
-@interface ZQuestionViewController ()
+@interface ZQuestionViewController () <UISearchDisplayDelegate, UISearchBarDelegate>
 @property (nonatomic, strong) NSArray *questions;
+@property (nonatomic, strong) NSArray *hot;
+@property (nonatomic, strong) NSArray *filteredQuestions;
 @end
 
 @implementation ZQuestionViewController
@@ -29,6 +31,12 @@
 {
     [super viewDidLoad];
 
+    self.hot = @[@{@"Title": @"浙江大学概况", @"Url": @"浙江大学概况.html"},
+                 @{@"Title": @"新生须知", @"Url": @"新生须知.html"},
+                 @{@"Title": @"常用电话", @"Url": @"常用电话.html"},
+                 @{@"Title": @"常用网络资源", @"Url": @"常用网络资源.html"},
+                 @{@"Title": @"游在杭州", @"Url": @"游在杭州.html"}
+                 ];
     [self initData];
 }
 
@@ -103,41 +111,96 @@
     }
 }
 
+- (void)filterWithText:(NSString *)text
+{
+    NSPredicate *predicate = [NSPredicate predicateWithFormat:@"Title contains[cd] %@ ", text];
+    self.filteredQuestions = [self.questions filteredArrayUsingPredicate:predicate];
+}
+
+#pragma mark - UISearchBar Delegate
+
+- (void)searchBar:(UISearchBar *)searchBar
+    textDidChange:(NSString *)searchText
+{
+    [self filterWithText:searchText];
+}
+
 #pragma mark - Table view data source
 
 - (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
 {
-    return 1;
+    if (tableView == self.tableView)
+        return 2;
+    else
+        return 1;
 }
 
 - (NSInteger)tableView:(UITableView *)tableView
  numberOfRowsInSection:(NSInteger)section
 {
-    return self.questions.count;
+    if (tableView == self.tableView)
+        return section == 1 ? self.questions.count : self.hot.count;
+    else
+        return self.filteredQuestions.count;
 }
 
-- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+- (NSString *)tableView:(UITableView *)tableView
+titleForHeaderInSection:(NSInteger)section
 {
-    static NSString *CellIdentifier = @"Cell";
-    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier forIndexPath:indexPath];
-    
-    // Configure the cell...
-    cell.textLabel.text = self.questions[indexPath.row][@"Title"];
-    
-    return cell;
+    if (tableView == self.tableView) {
+        return section == 0 ? @"热门问题" : @"所有问题";
+    }
+    return nil;
 }
 
+- (UITableViewCell *)tableView:(UITableView *)tableView
+         cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    if (tableView == self.tableView) {
+        static NSString *CellIdentifier = @"Cell";
+        UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier forIndexPath:indexPath];
+
+        // Configure the cell...
+        cell.textLabel.text = indexPath.section == 1 ? self.questions[indexPath.row][@"Title"] : self.hot[indexPath.row][@"Title"];
+
+        return cell;
+    }
+    else {
+        UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"SearchCell"];
+        if (!cell) {
+            cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault
+                                          reuseIdentifier:@"SearchCell"];
+            cell.selectionStyle = UITableViewCellSelectionStyleGray;
+        }
+        cell.textLabel.text = self.filteredQuestions[indexPath.row][@"Title"];
+
+        return cell;
+    }
+    return nil;
+}
+
+- (void)tableView:(UITableView *)tableView
+didSelectRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    if (tableView == self.searchDisplayController.searchResultsTableView) {
+        ZWebViewController *web = [self.storyboard instantiateViewControllerWithIdentifier:@"ZWebView"];
+        web.url = [NSURL fileURLWithPath:[[self htmlPath] stringByAppendingPathComponent:self.filteredQuestions[indexPath.row][@"Url"]]];
+        [self.navigationController pushViewController:web
+                                             animated:YES];
+    }
+}
 
 #pragma mark - Navigation
 
 // In a story board-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+- (void)prepareForSegue:(UIStoryboardSegue *)segue
+                 sender:(id)sender
 {
     // Get the new view controller using [segue destinationViewController].
     // Pass the selected object to the new view controller.
     NSIndexPath *indexPath = [self.tableView indexPathForSelectedRow];
     ZWebViewController *web = (ZWebViewController *)segue.destinationViewController;
-    web.url = [NSURL fileURLWithPath:[[self htmlPath] stringByAppendingPathComponent:self.questions[indexPath.row][@"Url"]]];
+    web.url = [NSURL fileURLWithPath:[[self htmlPath] stringByAppendingPathComponent:(indexPath.section == 1 ? self.questions : self.hot)[indexPath.row][@"Url"]]];
 }
 
 

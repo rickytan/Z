e402cca9e2e579eccaef2f463e3ffb288d9ba9f2
rickytan <ricky.tan.xin@gmail.com>
2014-03-21 13:16:49 +0800

    add feed parser

---
+++ ZWork/ZWork/ZParttimesViewController.h
@@ -0,0 +1,13 @@
+//
+//  ZParttimesViewController.h
+//  ZWork
+//
+//  Created by ricky on 14-3-21.
+//  Copyright (c) 2014年 Ricky. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ZParttimesViewController : UITableViewController
+
+@end

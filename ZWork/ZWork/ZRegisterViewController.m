479899bcbb2fa128b491d275aa7e798fa0018544
rickytan <ricky.tan.xin@gmail.com>
2014-03-23 01:23:35 +0800

    reg

---
+++ ZWork/ZWork/ZRegisterViewController.m
@@ -0,0 +1,38 @@
+//
+//  ZRegisterViewController.m
+//  ZWork
+//
+//  Created by ricky on 14-3-23.
+//  Copyright (c) 2014年 Ricky. All rights reserved.
+//
+
+#import "ZRegisterViewController.h"
+
+@interface ZRegisterViewController ()
+
+@end
+
+@implementation ZRegisterViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
